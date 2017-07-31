73298afe0c4880487f3eb0bce463b4614f0f0954
Victor Domingos <victordomingos@users.noreply.github.com>
2017-07-31 18:44:34 +0100

    mais detalhes

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,15 +1,21 @@
-#!/usr/bin/python3
-
 import requests 
 import sys
 import json
+import arrow
+from pprint import pprint
 
 
-# API provider: http://openweathermap.org/
-APIKEY = 'APIKEY' # replace with you API key
-LOCATION = 'Braga,pt'
+# API provider: please check http://openweathermap.org/
+APIKEY = 'APIKEY'
 API_URL = 'http://api.openweathermap.org/data/2.5/forecast'
 
+# Enter your location here (City, contry code)
+LOCATION = 'Esposende,pt'
+
+
+def dayNameFromWeekday(weekday):
+    days = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"]
+    return days[weekday] if 0 < weekday < len(days) else None
 
 def get_weather_data():
     try:            
@@ -25,16 +31,65 @@ def get_weather_data():
     except Exception as e:            
         print(e)
         sys.exit(1)
-    
 
 previsoes = get_weather_data()['list']
 
+
+data_anterior = ''
 for previsao in previsoes:
-    data = previsao['dt_txt'].replace(':00:00', 'h')
+    icone = ''
+    data, hora = previsao['dt_txt'].replace(':00:00', 'h').split()
     temperatura_int = int(previsao['main']['temp'])
     temperatura = str(temperatura_int)+'ºC'
-    tempo = previsao['weather'][0]['description']
+    tempo = previsao['weather'][0]['description'].title()
     #pressao_int = int(previsao['main']['pressure'])
     #pressao = str(pressao_int)+'hPa'
+    arr_data = arrow.get(data)
+    data_curta = arr_data.format('DD/MM')
     
-    print(data, temperatura, tempo)
+    if 'rain' in previsao.keys():
+        if '3h' in previsao['rain'].keys():
+            chuva = str(previsao['rain']['3h'])
+            icone = '🌧'
+            fchuva = float(chuva)
+            if fchuva < .75:
+                chuva = '❔'
+            elif .75 <= fchuva < 1.5:
+                chuva = '💧'
+            elif 1.5 <= fchuva < 1.5:
+                chuva = '💧💧'
+            elif 1.5 <= fchuva < 3:
+                chuva = '💧💧💧'
+            elif 1.5 <= fchuva < 3:
+                chuva = '💦💦☔️💦💦'
+            
+        else:
+            chuva = ''
+    else:
+        chuva = ''
+    
+    if data_anterior == '':
+        print('\nHoje ('+data_curta+')')
+    elif data == data_anterior:
+        pass
+    else:
+        dia_da_semana = dayNameFromWeekday(arr_data.weekday())
+        print('\n'+dia_da_semana+' ('+data_curta+')')
+
+
+    if tempo == 'Céu Claro':
+        tempo = 'Céu Limpo'
+        icone = '☀️'
+    elif tempo == 'Nuvens Quebrados':
+        tempo = 'Céu Muito Nublado'
+        icone = '☁️'
+
+
+    if ('Nuvens' in tempo):
+        icone = '☁️'
+    if 'Chuva' in tempo:
+        tempo = tempo + ' ' + chuva
+    
+    
+    print(' ', hora, temperatura, icone, tempo)
+    data_anterior = data
